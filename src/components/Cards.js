6918afbf8cac2e635b2980b64fbd6d96f09a39e2
Yessith Estiven Herrera Sanjuan <yessith.hs@gmail.com>
2022-05-25 13:00:17 -0500

    edit cat 2

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react'
-import { Button, Card } from 'react-bootstrap'
-import { useGetInput } from '../hooks/useGetInput'
+import { Card } from 'react-bootstrap'
+
 import '../style/cards.css'
-import { deleteCat, editCat } from '../utils/crud'
-import { Modal } from './Modal'
+import { deleteCat } from '../utils/crud'
+
 // import { Modal } from './Modal'
 
 export const Cards = ({ catData }) => {
-	const [show, setShow] = useState(false)
-	const { name, inputRef, getInput } = useGetInput()
-
-	const handleClose = () => setShow(false)
-	const handleShow = () => setShow(true)
 	return (
 		<div>
 			<Card className='m-auto' style={{ width: '18rem' }}>
@@ -30,26 +24,7 @@ export const Cards = ({ catData }) => {
 					<Card.Title>{catData ? catData.name : 'Gato'}</Card.Title>
 				</Card.Body>
 				<button onClick={() => deleteCat(catData.id)}>Delete</button>
-				<Button variant='primary' onClick={handleShow}>
-					Edit
-				</Button>
 			</Card>
-			<Modal show={show} onHide={handleClose}>
-				<Modal.Header closeButton>
-					<Modal.Title>Change Name Cat</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					<input onChange={getInput} type='text' ref={inputRef} />
-				</Modal.Body>
-				<Modal.Footer>
-					<Button variant='secondary' onClick={handleClose}>
-						Close
-					</Button>
-					<Button variant='primary' onClick={() => editCat(catData.id, name)}>
-						Save Changes
-					</Button>
-				</Modal.Footer>
-			</Modal>
 		</div>
 	)
 }
