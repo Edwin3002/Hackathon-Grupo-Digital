6202d563a81d7ab46495b8b6b5498f96ff0c2752
Edwin3002 <vargas11742@gmail.com>
2022-05-25 10:27:44 -0500

    btn get

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import {Card, ListGroup, ListGroupItem} from 'react-bootstrap'
+import { Button, Card, ListGroup, ListGroupItem } from 'react-bootstrap'
+import '../style/cards.css'
 
 export const Cards = () => {
 
@@ -17,29 +18,19 @@ export const Cards = () => {
 
     useEffect(() => {
         getData()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
 
     return (
         <div>
             <Card className='m-auto' style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={cat? cat[0].url: 'https://ih1.redbubble.net/image.3017788556.7359/st,small,507x507-pad,600x600,f8f8f8.jpg'} />
+                <div className='contImg'>
+                    <Card.Img className='cImg' variant="top" src={cat ? cat[0].url : 'https://ih1.redbubble.net/image.3017788556.7359/st,small,507x507-pad,600x600,f8f8f8.jpg'} />
+                </div>
                 <Card.Body>
                     <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem>Cras justo odio</ListGroupItem>
-                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                </ListGroup>
-                <Card.Body>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
+                    <Button variant="primary" onClick={()=> getData()}>Next</Button>
                 </Card.Body>
             </Card>
         </div>
